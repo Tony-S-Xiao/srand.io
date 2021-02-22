a683d63ae0425b021a72ad03ceff8e48d6cfa56a
TonyXiao <tony.s.xiao@gmail.com>
2021-02-21 16:05:15 -0800

    deploy

--- micro_entropy_weather/app.js
+++ micro_entropy_weather/app.js
@@ -22,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRouter);
 app.use('/random', indexRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
