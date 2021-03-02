73f7af23ab98abaf8a1ccbb1680f20a6d26f34e5
TonyXiao <tony.s.xiao@gmail.com>
2021-03-01 23:16:14 -0800

    front

--- front/src/components/RandomNumberDisplay.js
+++ front/src/components/RandomNumberDisplay.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import {CircularProgressbar} from 'react-circular-progressbar';
 
 class RandomNumberDisplay extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        let string = this.props.random.toString().padStart(64, '0');
         return (
-            <Card body bg='secondary' text='light' style={{borderRadius: 0}}>{this.props.random}</Card>
+            <Card body bg='dark' text='light' style={{borderRadius: 0, height:"200px", backgroundImage: "linear-gradient(0deg, #000000, #212529)"}}>
+                <Card.Title style={{borderRadius: 0,  width:"1000px", margin:"auto", fontSize:"26px", height: "100%", padding:"60px 0"}} className="text-center">
+                    {string}
+                </Card.Title>
+            </Card>
         );
     }
 }
---
+++ front/src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+export default class SingleCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Card text='dark' bg='light' style={{width:"70%", height: this.props.height,
+             borderRadius:10, margin:'auto', marginTop: 10, marginBottom: 10}}>
+                 <Card.Body style={{margin:'auto', width:'600px'}}>
+                    <Card.Title style={{height: '70px', fontSize: this.props.titleFontSize}}>
+                        {this.props.title}
+                    </Card.Title>
+                    <Card.Text>
+                        {this.props.children}
+                    </Card.Text>
+                 </Card.Body>
+            </Card>
+        );
+    }
+}
